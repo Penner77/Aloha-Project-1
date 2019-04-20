8c7e85771ac1e5351f19338587f50254a9d71568
Jeff <basilofbakerstreet@gmail.com>
2019-04-20 10:59:46 -0700

    Up to 600 @media first complete

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+document.addEventListener("DOMContentLoaded", function() { 
+    
+    const flkty = new Flickity( '.main-carousel', {
+        cellAlign: 'center',
+        wrapAround: true,
+        freeScroll: true
+      });
+
+}); //END OF DOCUMENT
+
+/* original code in this section:
+
+document.addEventListener("DOMContentLoaded", function() { 
+    
+    const flkty = new Flickity( '.main-carousel', {
+        cellAlign: 'left',
+        wrapAround: true,
+        freeScroll: true
+      });
+
+      */
