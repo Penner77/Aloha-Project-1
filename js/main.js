4e3bac91c1c92f16578149948c8cfc5cf4795df6
Jeff <basilofbakerstreet@gmail.com>
2019-04-22 09:02:17 -0700

    Final with submission button.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,43 @@
 
 
-document.addEventListener("DOMContentLoaded", function() { 
-    
-  const flkty = new Flickity( '.main-carousel', {
-      cellAlign: 'left',
-      wrapAround: true,
-      freeScroll: true,
-      groupCells: true,  
+document.addEventListener("DOMContentLoaded", function () {
+
+    const flkty = new Flickity('.main-carousel', {
+        cellAlign: 'left',
+        wrapAround: true,
+        freeScroll: true,
+        groupCells: true,
     });
 
     /* as found on https://www.formget.com/javascript-submit-form/ */
 
-    const subButton = document.getElementById("subScribe").submit;
-    button.addEventListener("click", function() {
-    function sucCess() {
-        const signedUp = document.getElementById("emailForm").value;
-        if (validation())
-        {
-        document.getElementById("subScribe").submit();
-        alert("Thank you for subscribing!");
-} 
-    }
+    const emailForm = document.getElementById("formId");
+    
+    // const subButton = document.getElementById("subScribe").submit;
+    emailForm.addEventListener("submit", function (event) {
+         event.preventDefault();
+            const emailInput = document.getElementById("emailForm").value;
+            if (emailInput != '') { 
+                alert("Thank you for subscribing!");
+            } else {
+                alert("Please enter your email!");
+            }
+        });
 
-function validation(){
-    const email = document.getElementById("emailForm").value;
-    const emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
-    if ( email ==='') {
-    alert("Please fill all fields...!");
-    return false;
-    } else if (!(email).match(emailReg)) {
-    alert("Invalid Email...!");
-    return false;
-    } else {
-    return true;
-    }
-    }
-});
+        // function validation() {
+        //     const email = document.getElementById("emailForm").value;
+        //     // const emailReg = /^([w-.]+@([w-]+.)+[w-]{2,4})?$/;
+        //     if (email === '') {
+        //         alert("Please fill all fields...!");
+        //         return false;
+        //     } else if (!(email).match(emailReg)) {
+        //         alert("Invalid Email...!");
+        //         return false;
+        //     } else {
+        //         return true;
+        //     }
+        // }
+    // });
 
 
 });
